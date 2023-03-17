bbeb0d6cc06111362a198b272414960188d3ef17
shubham shirke <shirkeshubham84@gmail.com>
2023-03-17 17:05:43 +0530

    Welcome to Address Book Program

---
+++ src/AddressBook.java
@@ -0,0 +1,15 @@
+
+public class AddressBook {
+    public static void main(String[] args){
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String state;
+        int zip;
+        int phoneNumber;
+        String email;
+    }
+
+
+    }
